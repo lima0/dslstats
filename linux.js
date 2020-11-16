bfe9314488d459474cab694068069fe72903339a
Selim <47898885+lima0@users.noreply.github.com>
2020-11-16 07:53:44 +0200

    added linux support

--- linux.js
+++ linux.js
@@ -1,4 +1,4 @@
-exports.gatewayIP = function(nicName) {
+exports.gatewayIP = function(_) {
   const gateway =
   execSync(`ip route | grep default | head -n1 | cut -f3 -d" "`);
   return String(gateway).replace('\n', '');
